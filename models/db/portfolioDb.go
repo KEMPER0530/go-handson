6dc87c93c269a7fedc613062213a2167c618b93a
Y.Akazawa <yoshi_akazawa_sto@RED-iMac-3.local>
2019-12-08 23:18:54 +0900

    メール登録用のAPI作成

--- models/db/portfolioDb.go
+++ models/db/portfolioDb.go
@@ -3,10 +3,14 @@ package db
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 
 	// constクラス
+	"github.com/google/uuid"
 	cnst "github.com/kemper0530/go-handson/common"
 
 	// エンティティ(データベースのテーブルの行に対応)
@@ -76,7 +80,7 @@ func FetchAllWorker() []entity.Work {
 	return work
 }
 
-// ログイン情報を取得する
+// カード情報を登録する
 func AddCardInfo(cardnumber string, cardname string, cardmonth int, cardyear int, cardcvv string) entity.CrdRgstRslt {
 	crdcardinfo := []entity.Crdcardinfo{}
 	crdRgstRslt := entity.CrdRgstRslt{}
@@ -116,4 +120,123 @@ func AddCardInfo(cardnumber string, cardname string, cardmonth int, cardyear int
 	close(db)
 
 	return crdRgstRslt
+}
+
+// お問合せ内容を登録する
+func SendMailRegist(to_email string, name string, text string, from_email string, personal_name string) entity.SendMailRslt {
+
+	mail_send_rslt := []entity.Mail_send_rslt{}
+	mst_ssmlknr := []entity.Mst_ssmlknr{}
+	sendMailRslt := entity.SendMailRslt{}
+
+	db := open()
+
+	// 送信連番の取得
+	count := cnst.ZERO
+	sendno1 := cnst.ZERO
+	db.Find(&mail_send_rslt).Count(&count)
+	sendno1 = count + 1
+	sendno2 := sendno1 + 1
+
+	// テナントIDの定義
+	tnntid := cnst.TNNTID
+
+	// msg_id1の生成
+	u, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Println(err)
+		// 登録失敗
+		sendMailRslt.Responce = cnst.JsonStatusOK
+		sendMailRslt.Result = cnst.TWO
+		return sendMailRslt
+	}
+	msgid1 := u.String()
+
+	// msg_id2の生成
+	u2, err2 := uuid.NewRandom()
+	if err2 != nil {
+		fmt.Println(err2)
+		// 登録失敗
+		sendMailRslt.Responce = cnst.JsonStatusOK
+		sendMailRslt.Result = cnst.TWO
+		return sendMailRslt
+	}
+	msgid2 := u2.String()
+
+	// 送信管理マスタの取得
+	db.First(&mst_ssmlknr)
+	subject := mst_ssmlknr[0].Subject
+	body := mst_ssmlknr[0].Body
+	replytitle := mst_ssmlknr[0].Replytitle
+	toreply := mst_ssmlknr[0].Toreply
+
+	// 文字列の置き換え　$1　→　登録名
+	_body := strings.Replace(body, "$1", name, -1)
+
+	// 日本時間へ変換
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	_time := time.Now().In(jst)
+
+	// insert メール送信情報(顧客用)
+	var mail_send_infIns = entity.Mail_send_inf{
+		Msg_id:        msgid1,
+		From_email:    from_email,
+		To_email:      to_email,
+		Subject:       subject,
+		Body:          _body,
+		Personal_name: personal_name,
+	}
+
+	// insert メール送信結果情報(顧客用)
+	var mail_send_rsltIns = entity.Mail_send_rslt{
+		Send_no:         sendno1,
+		Msg_id:          msgid1,
+		Tnnt_id:         tnntid,
+		Target_sys_type: strconv.Itoa(cnst.ONE),
+		Status:          strconv.Itoa(cnst.ZERO),
+		Server_id:       cnst.SERVID,
+		Priority:        cnst.ONE,
+		Send_reg_at:     _time,
+		Queue_remove:    strconv.Itoa(cnst.ZERO),
+		Updated_at:      _time,
+	}
+
+	// insert
+	db.Create(&mail_send_infIns)
+	db.Create(&mail_send_rsltIns)
+
+	// insert メール送信情報(送信者用)
+	mail_send_infIns = entity.Mail_send_inf{
+		Msg_id:        msgid2,
+		From_email:    from_email,
+		To_email:      toreply,
+		Subject:       replytitle,
+		Body:          text,
+		Personal_name: personal_name,
+	}
+
+	// insert メール送信結果情報(送信者用)
+	mail_send_rsltIns = entity.Mail_send_rslt{
+		Send_no:         sendno2,
+		Msg_id:          msgid2,
+		Tnnt_id:         tnntid,
+		Target_sys_type: strconv.Itoa(cnst.ONE),
+		Status:          strconv.Itoa(cnst.ZERO),
+		Server_id:       cnst.SERVID,
+		Priority:        cnst.ONE,
+		Send_reg_at:     _time,
+		Queue_remove:    strconv.Itoa(cnst.ZERO),
+		Updated_at:      _time,
+	}
+
+	// insert
+	db.Create(&mail_send_infIns)
+	db.Create(&mail_send_rsltIns)
+
+	sendMailRslt.Responce = cnst.JsonStatusOK
+	sendMailRslt.Result = cnst.ONE
+
+	close(db)
+
+	return sendMailRslt
 }
---
+++ models/entity/Mail_send_rslt.go
@@ -0,0 +1,19 @@
+package entity
+
+import "time"
+
+type Mail_send_rslt struct {
+	Send_no         int       `gorm:"primary_key" json:"send_no"`
+	Msg_id          string    `gorm:"type:char(36);not null" json:"msg_id"`
+	Tnnt_id         string    `gorm:"type:char(5);not null" json:"tnnt_id"`
+	Target_sys_type string    `gorm:"type:char(1);not null" json:"target_sys_type"`
+	Status          string    `gorm:"type:char(1);null" json:"status"`
+	Server_id       string    `gorm:"type:varchar(20);null" json:"server_id"`
+	Queue_id        string    `gorm:"type:varchar(20);null" json:"queue_id"`
+	Priority        int       `gorm:"null" json:"priority"`
+	Send_reg_at     time.Time `gorm:"null" json:"send_reg_at"`
+	Mta             string    `gorm:"type:varchar(5);null" json:"mta"`
+	Dsn_cd          string    `gorm:"type:char(5);null" json:"dsn_cd"`
+	Queue_remove    string    `gorm:"type:char(1);not null" json:"from_email"`
+	Updated_at      time.Time `gorm:"null" json:"updated_at"`
+}
