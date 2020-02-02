cbe9bb7b42412de327fb2b576c978f48eaf06868
Y.Akazawa <yoshi_akazawa_sto@RED-iMac-3.local>
2020-02-02 10:45:31 +0900

    bugfix

--- models/entity/Profile.go
+++ models/entity/Profile.go
@@ -1,8 +1,10 @@
 package entity
 
+import "time"
+
 type Profile struct {
-	Id       int    `gorm:"primary_key;not null"     json:"id"`
-	Heading  string `gorm:"type:varchar(100);null"   json:"heading"`
-	Lastdate string `gorm:"type:null"                json:"lastdate"`
-	History  string `gorm:"type:varchar(100);null"   json:"history"`
+	Id       int       `gorm:"primary_key;not null"     json:"id"`
+	Heading  string    `gorm:"type:varchar(100);null"   json:"heading"`
+	Lastdate time.Time `gorm:"type:null"                json:"lastdate"`
+	History  string    `gorm:"type:varchar(100);null"   json:"history"`
 }
